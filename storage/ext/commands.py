f20e01f6f3e41ab8293742c34702f067cbf46175
nezhdanchik <nezhdanchik@users.noreply.github.com>
2024-12-23 16:56:17 +0000

    ✅ Ready to clone and code.

--- storage/ext/commands.py
+++ storage/ext/commands.py
@@ -1,8 +1,8 @@
 import click
 
-from project_name.ext.auth import create_user
-from project_name.ext.database import db
-from project_name.models import Product
+from storage.ext.auth import create_user
+from storage.ext.database import db
+from storage.models import Product
 
 
 def create_db():
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
 import sys
 import pytest
 
-from project_name import create_app
-from project_name.ext.commands import populate_db
-from project_name.ext.database import db
+from storage import create_app
+from storage.ext.commands import populate_db
+from storage.ext.database import db
 
 
 @pytest.fixture(scope="session")
